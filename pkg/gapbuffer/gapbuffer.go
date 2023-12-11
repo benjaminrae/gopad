085fbf1409392530efbc78b7daf82c9f302c1982
Benjamin Rae <benjaminrae93@gmail.com>
2023-12-11 18:18:41 +0100

    feat: create GapBuffer

---
+++ pkg/gapbuffer/gapbuffer.go
@@ -0,0 +1,111 @@
+package gapbuffer
+
+type GapBuffer struct {
+	Buffer   []byte
+	Size     int
+	GapStart int
+	GapSize  int
+}
+
+func New(size int) GapBuffer {
+	gapBuffer := GapBuffer{
+		Buffer:   make([]byte, size),
+		Size:     size,
+		GapStart: 0,
+		GapSize:  size,
+	}
+
+	return gapBuffer
+}
+
+func (g *GapBuffer) Grow() {
+	newSize := g.Size * 2
+	newBuffer := make([]byte, newSize)
+
+	for i := 0; i < g.GapStart; i++ {
+		newBuffer[i] = g.Buffer[i]
+	}
+
+	for i := g.GapStart + g.GapSize; i < g.Size; i++ {
+		newBuffer[i] = g.Buffer[i]
+	}
+
+	g.Buffer = newBuffer
+	g.Size = newSize
+}
+
+func (g *GapBuffer) StepLeft() {
+	if g.GapStart == 0 {
+		return
+	}
+
+	gapEnd := g.GapStart + g.GapSize
+
+	g.Buffer[gapEnd-1] = g.Buffer[g.GapStart-1]
+
+	g.GapStart--
+
+	g.Buffer[g.GapStart] = byte(0)
+}
+
+func (g *GapBuffer) StepRight() {
+	if g.GapStart+g.GapSize == g.Size {
+		return
+	}
+
+	gapEnd := g.GapStart + g.GapSize
+
+	g.Buffer[g.GapStart] = g.Buffer[gapEnd]
+
+	g.GapStart++
+	g.Buffer[gapEnd] = byte(0)
+}
+
+func (g *GapBuffer) MoveLeft(distance int) {
+	for i := 0; i < distance; i++ {
+		g.StepLeft()
+	}
+}
+
+func (g *GapBuffer) MoveRight(distance int) {
+	for i := 0; i < distance; i++ {
+		g.StepRight()
+	}
+}
+
+func (g *GapBuffer) InsertRune(rune rune) {
+	if g.GapSize == 0 {
+		g.Grow()
+	}
+
+	g.Buffer[g.GapStart] = byte(rune)
+	g.GapStart++
+	g.GapSize--
+}
+
+func (g *GapBuffer) InsertString(string string) {
+	for _, rune := range string {
+		g.InsertRune(rune)
+	}
+}
+
+func (g *GapBuffer) DeleteLeft() {
+	if g.GapStart == 0 {
+		return
+	}
+
+	g.Buffer[g.GapStart] = byte(0)
+	g.GapSize++
+	g.GapStart--
+}
+
+func (g *GapBuffer) DeleteRight() {
+	gapEnd := g.GapStart + g.GapSize
+
+	if gapEnd == g.Size {
+		return
+	}
+
+	g.Buffer[gapEnd] = byte(0)
+	g.GapSize++
+}
